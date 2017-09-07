8fc56ff3441eb632dc05d9d9f3eeab31915359f1
Ariel Wald <15020608+AriTheGuitarMan@users.noreply.github.com>
2017-09-06 23:47:24 -0400

    final commits to assignment 3

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,15 +7,53 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    tenido = fs.readFileSync("./listings.json"),
+    json_lists = JSON.parse(tenido);
 
 /* Connect to your database */
-
+mongoose.connect('mongodb://ariel:database@ds123534.mlab.com:23534/terminator', function (err, res)
+    {
+        if(err)
+        {
+            console.log ('ERROR connecting to: mongodb://ariel:database@ds123534.mlab.com:23534/terminator' + err);
+        }
+        else{
+            console.log ('Succedded connecting to: mongodb://ariel:database@ds123534.mlab.com:23534/terminator');    
+        }
+    });
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+for(var jj = 0; jj < json_lists.entries.length; jj++)
+    {
+        var entry = json_lists.entries[jj];
+        entry["created_at"] = null;
+        entry["updated_at"] = null;
+        var modelonuevo = new Listing(entry);
+        modelonuevo.save(function(err) {
+            if (err)
+                throw err;//return handleError(err);
+        });
+    }
+
+/*fs.readFile('./listings.json','utf8', function(err, data){
+    if(err)
+        throw err;
+    var json = JSON.parse(data);
+    Listing.collection.insert(json, function(err)
+    {
+        if(err)
+            throw err;
+        //console.log(data);
+    }) ;      
+});
+
+//var db = mongoose.connection;
+//db.on{'error', console.error.bind};
+*/
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -5,6 +5,17 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+    
+    
+    Listing.find({ name: 'Library West' }, function(err, ldata) {
+    if (err) throw err;
+
+    // object of the user
+    console.log(data);
+});
+
+    
+    
 };
 var removeCable = function() {
   /*
@@ -12,17 +23,38 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+    
+    Listing.find({ code: 'CABL' }, function(err, data) {
+    if (err) throw err;
+
+    // object of the user
+    console.log(data);
+    
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+        
+    Listing.findOneAndUpdate({ name: 'Phelps Laboratory' }, {address: 'Phelps Lab, Gainesville, Fl 32603'}, function(err, listing) {
+        if(err)
+            throw err;
+        console.log(listing);
+    });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+    /* 
+      Retrieve all listings in the database, and log them to the console. 
    */
+      
+    Listing.find({}, function(err, entries) {
+    if (err) throw err;
+
+    // object of all the users
+    console.log(entries);
+});
+    
 };
 
 findLibraryWest();
