d30527c6847a98a623f25a5a18699ae7f2d66c9b
Ariel Wald <15020608+AriTheGuitarMan@users.noreply.github.com>
2017-09-19 21:21:48 -0400

    commit8

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -103,7 +103,7 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
                 $state.go('listings.list', { successMessage: 'Listing succesfully updated!' });
               }, function(error) {
                 //otherwise display the error
-                $scope.error = 'Unable to save listing!\n' + (error);
+                $scope.error = 'Unable to save listing!\n' + error;
               });
         
         
@@ -118,13 +118,13 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         //$scope.error = null;
       /* */
         var id = $stateParams.listingId;
-        Listings.delete(listing)
+        Listings.delete(id)
               .then(function(response) {
                 //if the object is successfully saved redirect back to the list page
                 $state.go('listings.list', { successMessage: 'Listing succesfully deleted!' });
               }, function(error) {
                 //otherwise display the error
-                $scope.error = 'Unable to delete listing!\n' + (error);
+                $scope.error = 'Unable to delete listing!\n' + error;
               });        
         
     };
